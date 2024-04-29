889e0323b98db3fe8909d49d04ca4b81bf0e4cf4
Arun Venkatesh S <arunvenkatesh2909@gmail.com>
2024-04-29 17:27:29 +0530

    Profile

--- src/Component/Ehome.jsx
+++ src/Component/Ehome.jsx
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Footer from './Footer'
 import EmployeeHeader from './EmployeeHeader'
+import { getEmployee } from '../Service/EmployeeService';
 
-const ehome = () => {
+const Ehome = () => {
+  const [employee, setEmployee] = useState([]);
+  const empId = sessionStorage.getItem("id");
+
+  useEffect(() => {
+      getEmployee(empId)
+        .then(response => {
+          setEmployee(response.data);
+        })
+        .catch(error => {
+          console.error('Error fetching employee data:', error);
+        });
+  }, []);
+
   return (
     <div>
-    <EmployeeHeader></EmployeeHeader>
-    <div>ehome</div>
-    <Footer></Footer>
+      <EmployeeHeader></EmployeeHeader>
+      <div><h3>{employee.empid}</h3>
+      <h3>{employee.empName}</h3>
+      <h3>{employee.role}</h3>
+      <h3>{employee.email}</h3>
+      </div>
+      <Footer></Footer>
     </div>
-  )
-}
+  );
+};
 
-export default ehome
+export default Ehome;
--- src/Component/ManagerHome.jsx
+++ src/Component/ManagerHome.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ManagerHeader from './ManagerHeader'
 import Footer from './Footer'
+import { getEmployee } from '../Service/EmployeeService';
+
 
 
 
 const ManagerHome = () => {
+
+  const [employee, setEmployee] = useState([]);
+  const empId = sessionStorage.getItem("id");
+
+  useEffect(() => {
+      getEmployee(empId)
+        .then(response => {
+          setEmployee(response.data);
+        })
+        .catch(error => {
+          console.error('Error fetching employee data:', error);
+        });
+  }, []);
+
   return (
     <>
     <ManagerHeader></ManagerHeader>
+    <div><h3>{employee.empid}</h3>
+      <h3>{employee.empName}</h3>
+      <h3>{employee.role}</h3>
+      <h3>{employee.email}</h3>
+      </div>
     <Footer></Footer>
     </>
   )
